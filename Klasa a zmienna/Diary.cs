46581d1bc47dcaae04db332b468c24ccae889308
Paweł Bień <paw1345@gmail.com>
2018-10-27 11:25:28 +0100

    Klasa a zmienna ćwiczenia

---
+++ Klasa a zmienna/Diary.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Klasa_a_zmienna
+{
+    class Diary
+    {
+        
+        List<float> ratings = new List<float>();
+
+
+        public void AddRating(float rating)
+        {
+            ratings.Add(rating);
+        }
+
+        public float CalculateAverage()
+        {
+            float sum = 0, avg = 0;
+
+            foreach (var rating in ratings)
+            {
+                sum = sum + rating;
+            }
+
+            avg = sum / ratings.Count();
+
+            return avg;
+            
+        }
+
+        public float GiveMaxRating()
+        {
+            return ratings.Max();
+
+           
+        }
+
+        public float GiveMinRating()
+        {
+            return ratings.Min();
+
+
+        }
+
+    }
+
+}
---
+++ Klasa a zmienna/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klasa_a_zmienna
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Diary diary = new Diary();
+            diary.AddRating(3);
+            diary.AddRating(5);
+            Diary diary2 = new Diary();
+            diary.AddRating(8);
+
+
+        }
+
+    }
+}
