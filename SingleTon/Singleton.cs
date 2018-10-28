860407072b54149dd5c83cc428588ab5fc90fcf8
Paweł Bień <paw1345@gmail.com>
2018-10-28 22:00:04 +0100

    SingleTon

---
+++ SingleTon/Singleton.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleTon
+{
+    public class Singleton
+    {
+        public String name;
+        private static Singleton instance;
+        
+        private Singleton()
+        {
+
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Singleton();
+                }
+                return instance;
+            }
+        }
+    }
+}
